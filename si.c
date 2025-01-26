5f24a17356c09eb9c4c36f463e94e0d22dd00942
j2o1e25 <joe.thomas0502@gmail.com>
2025-01-26 17:33:58 +0530

    Initial commit

---
+++ si.c
@@ -0,0 +1,4 @@
+/*5. Simple Interest Calculation
+Write an algorithm, pseudocode, and flowchart to calculate simple interest based on the formula SI = (P × R × T) / 100, where P is the principal amount, R is the rate of interest, and T is the time in years.
+
+*/
